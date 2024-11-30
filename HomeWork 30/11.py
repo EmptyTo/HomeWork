5dda44646610d9a1dac12def7fc24bb4ceaeb83a
User <valeratelyatnikov@gmail.com>
2024-11-30 20:29:03 +0600

    final

---
+++ HomeWork 30/11.py
@@ -0,0 +1,6 @@
+onename = ('Python') #название курса
+twoname = (12) #кол-во выполненых домашних заданий
+threename = (1.5) #кол-во затраченных часов
+Fourname = (threename / twoname)
+
+print(onename, 'всего задач:',twoname, 'затраченно часов:', threename, 'среднее время выполнения:', Fourname)
